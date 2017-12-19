f50cad29c76082bf9729a1a9a7ce9c8824871336
Bruno Correia <brunocs@msn.com>
2017-12-19 01:56:09 -0200

    Home Screen: Text Inputs (beta)

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+
+import styles from './styles';
+
+const InputWithButton = (props) => {
+  const { onPress, buttonText, editable } = props;
+  return (
+    <View style={styles.container}>
+      <TouchableHighlight style={styles.buttonContainer} onPress={onPress}>
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableHighlight>
+      <View style={styles.border} />
+      <TextInput style={styles.input} {...props} />
+    </View>
+  );
+};
+
+InputWithButton.propTypes = {
+  onPress: PropTypes.func,
+  buttonText: PropTypes.string,
+  editable: PropTypes.bool,
+};
+
+InputWithButton.defaultProps = {
+  onPress: null,
+  buttonText: null,
+  editable: true,
+};
+
+export default InputWithButton;
